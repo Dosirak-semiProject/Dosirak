2d57f9d2caa17cd8be3de6367f5dc5e6473567be
won1999won <won1999won6@gmail.com>
2024-04-09 15:06:54 +0900

    kim jung won

--- Dosirak/src/main/java/com/ohgiraffers/dosirak/Application.java
+++ Dosirak/src/main/java/com/ohgiraffers/dosirak/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hi123");
+        System.out.println("hi kim jung");
         SpringApplication.run(Application.class, args);
     }
 }
