cef65d95d60bc1d146dac9b6c4609e2a58aa9997
sunguh0904 <sunguh0904@gmail.com>
2024-04-09 15:02:57 +0900

    성우현2

--- Dosirak/src/main/java/com/ohgiraffers/test/test.java
+++ Dosirak/src/main/java/com/ohgiraffers/test/test.java
@@ -2,6 +2,6 @@ package com.ohgiraffers.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("깃푸시 테스트");
+        System.out.println("깃푸시 테스트2");
     }
 }
