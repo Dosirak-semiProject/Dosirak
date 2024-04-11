85586039b5d640c0c704e965372e4aabf3c79079
LXXDJ <joker96.dj@gmail.com>
2024-04-11 09:22:59 +0900

    test

--- Dosirak/src/main/java/com/ohgiraffers/dosirak/config/Application.java
+++ Dosirak/src/main/java/com/ohgiraffers/dosirak/config/Application.java
@@ -5,11 +5,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("머지테스트");
         SpringApplication.run(Application.class, args);
     }
 }
