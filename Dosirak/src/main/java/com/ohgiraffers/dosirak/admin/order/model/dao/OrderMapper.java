2154956cda445b897072fd1abdc30dc48c2f1cb4
sunguh0904 <sunguh0904@gmail.com>
2024-04-12 17:17:19 +0900

    리펀드 DTO 추가

--- Dosirak/src/main/java/com/ohgiraffers/dosirak/admin/order/model/dao/OrderMapper.java
+++ Dosirak/src/main/java/com/ohgiraffers/dosirak/admin/order/model/dao/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.dosirak.admin.order.model.dao;
 
 import com.ohgiraffers.dosirak.admin.order.model.dto.OrderDTO;
+import com.ohgiraffers.dosirak.admin.order.model.dto.RefundDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,5 +10,5 @@ import java.util.List;
 public interface OrderMapper {
     List<OrderDTO> AllOrderList();
 
-    List<OrderDTO> AllRefundList();
+    List<RefundDTO> AllRefundList();
 }
