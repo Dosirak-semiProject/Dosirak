b14970fd0e5690d5f919d6f20c18be1aca16d236
LXXDJ <joker96.dj@gmail.com>
2024-04-23 17:04:45 +0900

    admin db 추가

--- Dosirak/src/main/resources/static/admin/assets/demo/chart-area-demo.js
+++ Dosirak/src/main/resources/static/admin/assets/demo/chart-area-demo.js
@@ -2,19 +2,14 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// 오늘 기준으로 일주일 날짜 가져오기
-let today = ('0' + new Date().getDate()).slice(-2);
-let dayArr = new Array();
-for(let i= today-7; i <= today; i++){ dayArr.push(i); }
-
 // Area Chart Example
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: dayArr,
+    labels: joinDates,
     datasets: [{
-      label: "Sessions",
+      label: "신규회원",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -25,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: countDates,
     }],
   },
   options: {
@@ -44,7 +39,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
+          max: 10,
           maxTicksLimit: 5
         },
         gridLines: {
