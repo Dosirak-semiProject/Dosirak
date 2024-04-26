0bc7085386713be908b10ab2e7466b73426dad8d
sunguh0904 <sunguh0904@gmail.com>
2024-04-26 11:12:08 +0900

    커밋

---
+++ Dosirak/src/main/java/com/ohgiraffers/dosirak/admin/order/controller/OrderRestController.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.dosirak.admin.order.controller;
+
+import com.ohgiraffers.dosirak.admin.order.model.dto.OrderDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/admin/*")
+public class OrderRestController {
+    Map<String, Object> response = new HashMap<>();
+
+    @PostMapping("order-list-selected")
+    public ResponseEntity<Map<String, Object>> orderListSelected(@RequestBody OrderDTO status) {
+//        비동기 구현 중
+        System.out.println("@@@@selected = " + status);
+        try {
+            response.put("message", "주문상태 변경 성공");
+            response.put("success", true);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("message", "주문상태 변경 실패");
+            response.put("success", false);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+}
